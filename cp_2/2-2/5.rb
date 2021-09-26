eaf21f3851e0c7605927128ad74871c42f9a713e
RyoKitatani <ryo0929m@gmail.com>
2021-09-27 08:24:12 +0900

    finished_2-2-6

---
+++ cp_2/2-2/5.rb
@@ -0,0 +1,10 @@
+# 変数などの名前を識別子という。予約語をそれらとして使用は避ける
+
+#予約語
+BEGIN    class    ensure   nil      self     when
+END      def      false    not      super    while
+alias    defined? for      or       then     yield
+and      do       if       redo     true     __LINE__
+begin    else     in       rescue   undef    __FILE__
+break    elsif    module   retry    unless   __ENCODING__
+case     end      next     return   until
