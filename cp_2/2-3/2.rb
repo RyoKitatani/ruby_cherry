f3bf175cbe7debddf63da0219cbd8b044571b494
RyoKitatani <ryo0929m@gmail.com>
2021-09-28 09:31:12 +0900

    finished_2-3-2

---
+++ cp_2/2-3/2.rb
@@ -0,0 +1,16 @@
+# 文字の比較
+p 'ruby' == 'ruby'
+p 'ruby' == 'Ruby'
+p 'ruby' != 'perl'
+p 'ruby' != 'ruby'
+
+p '------------------'
+
+#大小の比較
+p 'a' < 'b'
+p 'a' < 'A'
+p 'a' > 'A'
+p 'abc' < 'def'
+p 'abc' < 'ab'
+p 'abc' < 'abcd'
+p 'あいうえお' < 'かきくけこ'
