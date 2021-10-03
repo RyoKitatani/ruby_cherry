8c99d9e18f95ece9c523b138a17b7591e73624c8
RyoKitatani <ryo0929m@gmail.com>
2021-10-03 12:54:06 +0900

    finished2-10-3

---
+++ cp_2/2-10/3.rb
@@ -0,0 +1,38 @@
+# unless文
+status = 'error'
+if status != 'ok'
+  p '何か異常はありますか'
+else
+  p '異常はありません'
+end
+
+status = 'ok'
+unless status == 'ok'
+ p '何か異常はありますか'
+else
+ p '正常です'
+end
+
+
+# if同様unlessの戻り値を変数に代入することが可能
+status = 'error'
+message =
+unless status == 'ok'
+  '何か異常はありますか'
+else
+  '正常です'
+end
+
+p message
+
+# unlessを就職しとして使う
+p '何か異常があります' unless status == 'ok'
+
+# thenを入れることも可能
+unless status == 'ok' then
+  '何か異常があります'
+end
+
+
+
+
